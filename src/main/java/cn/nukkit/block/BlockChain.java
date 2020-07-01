06b37a74bc590d8b531868f2d2f776e2c15379a6
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-07-01 22:43:39 +0900

    Fix Chain Item Drop

--- src/main/java/cn/nukkit/block/BlockChain.java
+++ src/main/java/cn/nukkit/block/BlockChain.java
@@ -2,6 +2,7 @@ package cn.nukkit.block;
 
 import cn.nukkit.item.Item;
 import cn.nukkit.item.ItemBlock;
+import cn.nukkit.item.ItemChain;
 import cn.nukkit.item.ItemTool;
 import cn.nukkit.utils.BlockColor;
 
@@ -49,7 +50,7 @@ public class BlockChain extends BlockThin {
 
     @Override
     public Item toItem() {
-        return new ItemBlock(this, 0);
+        return new ItemChain();
     }
 
     @Override
