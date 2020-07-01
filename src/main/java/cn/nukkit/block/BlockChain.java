1cffb1a7689e513163c0fd32f3963c13d4695cfa
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-07-01 18:21:42 +0900

    Create BlockChain.java

---
+++ src/main/java/cn/nukkit/block/BlockChain.java
@@ -0,0 +1,75 @@
+package cn.nukkit.block;
+
+import cn.nukkit.item.Item;
+import cn.nukkit.item.ItemBlock;
+import cn.nukkit.item.ItemTool;
+import cn.nukkit.utils.BlockColor;
+
+/**
+ * BlockChain.java was made by using BlockIronBars.java
+ */
+/**
+ * Created on 2015/12/6 by xtypr.
+ * Package cn.nukkit.block in project Nukkit .
+ */
+public class BlockChain extends BlockThin {
+
+    public BlockChain() {
+    }
+
+    @Override
+    public String getName() {
+        return "Chain";
+    }
+
+    @Override
+    public int getId() {
+        return CHAIN;
+    }
+
+    @Override
+    public double getHardness() {
+        return 5;
+    }
+
+    @Override
+    public int getWaterloggingLevel() {
+        return 1;
+    }
+
+    @Override
+    public double getResistance() {
+        return 6;
+    }
+
+    @Override
+    public int getToolType() {
+        return ItemTool.TYPE_PICKAXE;
+    }
+
+    @Override
+    public Item toItem() {
+        return new ItemBlock(this, 0);
+    }
+
+    @Override
+    public Item[] getDrops(Item item) {
+        if (item.isPickaxe() && item.getTier() >= ItemTool.TIER_WOODEN) {
+            return new Item[]{
+                    this.toItem()
+            };
+        } else {
+            return new Item[0];
+        }
+    }
+
+    /*@Override
+    public BlockColor getColor() {
+        return BlockColor.IRON_BLOCK_COLOR;
+    }*/
+
+    @Override
+    public boolean canHarvestWithHand() {
+        return false;
+    }
+}
