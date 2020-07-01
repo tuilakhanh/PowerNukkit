fa965d4a85f2f4a03ff9689d6609c10cf2860d2d
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-07-01 18:27:12 +0900

    Add Chain to Item.java

--- src/main/java/cn/nukkit/item/Item.java
+++ src/main/java/cn/nukkit/item/Item.java
@@ -321,6 +321,8 @@ public class Item implements Cloneable, BlockID, ItemID {
 
             list[HONEYCOMB] = ItemHoneycomb.class; //736
             list[HONEY_BOTTLE] = ItemHoneyBottle.class; //737
+            
+            list[CHAIN] = ItemChain.class; //758
 
             for (int i = 0; i < 256; ++i) {
                 if (Block.list[i] != null) {
