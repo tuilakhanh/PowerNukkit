e22d665794768bd8a7b52ded4308be9d0e95a934
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-07-02 11:43:18 +0900

    Add Crimson Planks

---
+++ src/main/java/cn/nukkit/block/BlockCrimsonPlanks.java
@@ -0,0 +1,41 @@
+package cn.nukkit.block;
+
+import cn.nukkit.item.ItemTool;
+import cn.nukkit.utils.BlockColor;
+
+public class CrimsonPlanks extends BlockSolid {
+
+    public CrimsonPlanks() {
+        this(0);
+    }
+
+    public CrimsonPlanks(int meta) {
+        super(meta);
+    }
+
+    @Override
+    public int getId() {
+        return CRIMSON_PLANKS;
+    }
+
+    @Override
+    public double getHardness() {
+        return 2;
+    }
+
+    @Override
+    public double getResistance() {
+        return 3;
+    }
+    
+    @Override
+    public int getToolType() {
+        return ItemTool.TYPE_AXE;
+    }
+
+    /*@Override
+    public BlockColor getColor() {
+        return BlockColor.BLACK_BLOCK_COLOR;
+    }*/
+
+}
