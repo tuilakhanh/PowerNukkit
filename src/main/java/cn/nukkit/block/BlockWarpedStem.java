950dcc493421e574dc44b2d3775875ab8a9986bc
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-07-02 22:28:24 +0900

    Create BlockWarpedStem.java

---
+++ src/main/java/cn/nukkit/block/BlockWarpedStem.java
@@ -0,0 +1,98 @@
+package cn.nukkit.block;
+
+import cn.nukkit.Player;
+import cn.nukkit.item.Item;
+import cn.nukkit.item.ItemBlock;
+import cn.nukkit.item.ItemTool;
+import cn.nukkit.math.BlockFace;
+
+/**
+ * BlockWarpedStem.java was made by using BlockWood.java
+ */
+/**
+ * author: MagicDroidX
+ * Nukkit Project
+ */
+public class BlockWarpedStem extends BlockSolid 
+
+    public BlockWarpedStem() {
+    }
+    
+    @Override
+    public int getId() {
+        return WARPED_STEM;
+    }
+
+    @Override
+    public double getHardness() {
+        return 1;
+    }
+
+    @Override
+    public double getResistance() {
+        return 2;
+    }
+
+    @Override
+    public String getName() {
+        return "Warped Stem";
+    }
+
+    @Override
+    public int getBurnChance() {
+        return 0;
+    }
+
+    @Override
+    public int getBurnAbility() {
+        return 0;
+    }
+
+    @Override
+    public boolean place(Item item, Block block, Block target, BlockFace face, double fx, double fy, double fz, Player player) {
+        short[] faces = new short[]{
+                0,
+                0,
+                0b1000,
+                0b1000,
+                0b0100,
+                0b0100
+        };
+        
+        //this.setDamage(((this.getDamage() & 0x03) | faces[face.getIndex()]));
+        this.getLevel().setBlock(block, this, true, true);
+
+        return true;
+    }
+    
+    @Override
+    public boolean canBeActivated() {
+        return true;
+    }
+    
+    @Override
+    public boolean onActivate(Item item, Player player) {
+        if (item.isAxe()) {
+            Block strippedBlock = Block.get(STRIPPED_WARPED_STEM);
+            item.useOn(this);
+            this.level.setBlock(this, strippedBlock, true, true);
+            return true;
+        }
+        return false;
+    }
+    
+    @Override
+    public Item toItem() {
+            return new ItemBlock(this);
+    }
+
+    @Override
+    public int getToolType() {
+        return ItemTool.TYPE_AXE;
+    }
+
+    /*@Override
+    public BlockColor getColor() {
+                return BlockColor.BLACK_BLOCK_COLOR;
+    }*/
+}
