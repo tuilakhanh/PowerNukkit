3b426c988722adad705ffe9cf92eaa1a0c1b52f3
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-07-02 11:46:37 +0900

    Add Crimson Planks, Warped Planks to Block ID

--- src/main/java/cn/nukkit/block/BlockID.java
+++ src/main/java/cn/nukkit/block/BlockID.java
@@ -412,5 +412,8 @@ public interface BlockID {
     int BEEHIVE = 474;
     int HONEY_BLOCK = 475;
     int HONEYCOMB_BLOCK = 476;
-
+    
+    int CRIMSON_PLANKS  = 497;
+    int WARPED_PLANKS = 498;
+    
 }
