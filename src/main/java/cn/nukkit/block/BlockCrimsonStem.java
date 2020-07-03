05c8f7df49ab8ab32bd9be3125fea00d7340a70a
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-07-03 10:05:49 +0900

    Import

--- src/main/java/cn/nukkit/block/BlockCrimsonStem.java
+++ src/main/java/cn/nukkit/block/BlockCrimsonStem.java
@@ -5,6 +5,7 @@ import cn.nukkit.item.Item;
 import cn.nukkit.item.ItemBlock;
 import cn.nukkit.item.ItemTool;
 import cn.nukkit.math.BlockFace;
+import cn.nukkit.utils.Faceable;
 
 /**
  * BlockCrimsonStem.java was made by using BlockWood.java
