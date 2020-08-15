bc7cdd1178e3ef30b74c38dd6f77af2eccb3414b
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-08-15 19:55:33 +0900

    Fix

--- src/main/java/cn/nukkit/entity/item/EntityMinecartCommandBlock.java
+++ src/main/java/cn/nukkit/entity/item/EntityMinecartCommandBlock.java
@@ -6,6 +6,7 @@ import cn.nukkit.entity.Entity;
 import cn.nukkit.item.Item;
 import cn.nukkit.level.format.FullChunk;
 import cn.nukkit.math.Vector3;
+import cn.nukkit.nbt.tag.CompoundTag;
 import cn.nukkit.utils.MinecartType;
 
 /**
