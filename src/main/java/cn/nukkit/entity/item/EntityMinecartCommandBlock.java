2aceba179ce7aa5c6a8a4d626a9b38e31a38689b
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-08-15 19:37:33 +0900

    Create EntityMinecartCommandBlock

---
+++ src/main/java/cn/nukkit/entity/item/EntityMinecartCommandBlock.java
@@ -0,0 +1,62 @@
+package cn.nukkit.entity.item;
+
+import cn.nukkit.Player;
+import cn.nukkit.block.Block;
+import cn.nukkit.entity.Entity;
+import cn.nukkit.item.Item;
+import cn.nukkit.level.format.FullChunk;
+import cn.nukkit.math.Vector3;
+import cn.nukkit.utils.MinecartType;
+
+/**
+ * Created by good777LUCKY
+ */
+public class EntityMinecartCommandBlock extends EntityMinecartAbstract {
+
+    public static final int NETWORK_ID = 100;
+    
+    // TODO: Add Functionality
+    
+    public EntityMinecartHopper(FullChunk chunk, CompoundTag nbt) {
+        super(chunk, nbt);
+        setDisplayBlock(Block.get(Block.COMMAND_BLOCK), false);
+    }
+    
+    @Override
+    public MinecartType getType() {
+        return MinecartType.valueOf(6);
+    }
+    
+    @Override
+    public boolean isRideable(){
+        return false;
+    }
+    
+    @Override
+    public int getNetworkId() {
+        return NETWORK_ID;
+    }
+    
+    @Override
+    public boolean mountEntity(Entity entity, byte mode) {
+        return false;
+    }
+    
+    @Override
+    public boolean onInteract(Player player, Item item, Vector3 clickedPos) {
+        // TODO: Add UI
+        return false;
+    }
+    
+    @Override
+    public void initEntity() {
+        super.initEntity();
+        // TODO: Add Functionality
+    }
+    
+    @Override
+    public void saveNBT() {
+        super.saveNBT();
+        // TODO: Add Functionality
+    }
+}
