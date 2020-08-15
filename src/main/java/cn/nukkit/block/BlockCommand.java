78bc480d219ff192aa36cc4253ed2fca1c0f7243
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-08-15 18:43:12 +0900

    Create BlockCommand

---
+++ src/main/java/cn/nukkit/block/BlockCommand.java
@@ -0,0 +1,24 @@
+package cn.nukkit.block;
+
+/**
+ * Created by good777LUCKY
+ */
+public class BlockCommand extends BlockCommandBase {
+
+    @Override
+    public String getName() {
+        return "Command Block";
+    }
+    
+    @Override
+    public int getId() {
+        return COMMAND_BLOCK;
+    }
+    
+    @Override
+    public BlockColor getColor() {
+        return BlockColor.BROWN_BLOCK_COLOR;
+    }
+    
+    // TODO: Add Functionality
+}
